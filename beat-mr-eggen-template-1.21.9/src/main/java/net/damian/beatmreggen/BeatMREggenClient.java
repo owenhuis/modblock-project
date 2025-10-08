7cda9e1cac88acddbe3157d3d5f8ed9a709392fe
Damian <99075982@mydavinci.nl>
2025-10-08 13:02:29 +0200

    Fabric Modding Tutorial - Minecraft 1.20: Workspace Setup | #1
    video workspace set up complet(needs to be done for every developer)

---
+++ beat-mr-eggen-template-1.21.9/src/main/java/net/damian/beatmreggen/BeatMREggenClient.java
@@ -0,0 +1,10 @@
+package net.damian.beatmreggen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeatMREggenClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
