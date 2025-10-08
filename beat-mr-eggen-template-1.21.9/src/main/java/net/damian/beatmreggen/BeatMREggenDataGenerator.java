950a5e295b919ffbf40d28f41f2a2317911fa2c2
Damian <99075982@mydavinci.nl>
2025-10-08 09:32:03 +0200

    fabric template
    fabric template voor de mod

---
+++ beat-mr-eggen-template-1.21.9/src/main/java/net/damian/beatmreggen/BeatMREggenDataGenerator.java
@@ -0,0 +1,11 @@
+package net.damian.beatmreggen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BeatMREggenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
