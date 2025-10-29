5e572c519f1edfbefc623b809077ff06488fbf7c
Damian <99075982@mydavinci.nl>
2025-10-29 09:46:23 +0100

    mr eggen bb/java file

--- beat-mr-eggen-template-1.21.9/src/main/java/net/damian/beatmreggen/entity/custom/MREggenEntity.java
+++ beat-mr-eggen-template-1.21.9/src/main/java/net/damian/beatmreggen/entity/custom/MREggenEntity.java
@@ -1,4 +1,42 @@
 package net.damian.beatmreggen.entity.custom;
 
-public class MREggenEntity {
+import net.minecraft.entity.AnimationState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.AnimalMateGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.TemptGoal;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class MREggenEntity extends AnimalEntity {
+
+    public final AnimationState idleAnimationState= new AnimationState();
+    private int idleAnimationTimeout = 0;
+
+    public MREggenEntity(EntityType<? extends AnimalEntity> entityType, World world ) {
+        super(entityType, world);
+    }
+
+    @Override
+    protected void initGoals() {
+        this.goalSelector.add(0,new SwimGoal(this));
+
+        this.goalSelector.add(1, new AnimalMateGoal(this, 1.150));
+        this.goalSelector.add(2, new TemptGoal(this, 1.250, Ingredient.ofItems(ModItems.Cauliflower), false))
+
+    }
+
+    @Override
+    public boolean isBreedingItem(ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public @Nullable PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return null;
+    }
 }
